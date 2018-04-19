a6ada34931da532ffb8a4d0bc7e8d430434d8173
Francis <Yuweh@users.noreply.github.com>
2018-04-19 09:09:53 +0800

    updated

--- CRASHLYTICS/SampleCODE.swift
+++ CRASHLYTICS/SampleCODE.swift
@@ -1,1 +1,23 @@
 ## SampleCODe will be provided here
+
+
+FOR APP Delegate under func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {}
+        // for Crashlytics
+        Fabric.with([Crashlytics.self])
+
+For Info.Plist
+
+      <key>Fabric</key>
+  <dict>
+    <key>APIKey</key>
+    <string>858fc11e4a5e557dd6cc99d2288d54f36fee4741</string>
+    <key>Kits</key>
+    <array>
+      <dict>
+        <key>KitInfo</key>
+        <dict/>
+        <key>KitName</key>
+        <string>Crashlytics</string>
+      </dict>
+    </array>
+  </dict>
